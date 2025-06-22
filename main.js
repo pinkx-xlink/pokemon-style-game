7c645eb70f86886ec6b8efd03f9285d540e87392
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-06-22 19:59:31 -0400

    Create the canvas and add background colors

---
+++ main.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('canvas');
+// "c" stands for "context"
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+console.log(c);
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height);
