542358a849065ce39135b36b1a176693a3085a52
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-06-22 21:47:49 -0400

    Make the player move up, down, left, or right depending on if a, s, d, or w is pressed

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ class Sprite {
     }
 
     draw() {
-        c.drawImage(this.image, -1050, -570);
+        c.drawImage(this.image, this.position.x, this.position.y);
     }
 }
 
@@ -38,6 +38,21 @@ const background = new Sprite({
     },
     image: image
 });
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
 
 function animate() {
     window.requestAnimationFrame(animate);
@@ -56,6 +71,11 @@ function animate() {
     playerImage.width / 4,
     playerImage.height
     )
+
+   if (keys.w.pressed) background.position.y += 3;
+   else if (keys.a.pressed) background.position.x += 3;
+   else if (keys.s.pressed) background.position.y -= 3;
+   else if (keys.d.pressed) background.position.x -= 3;
 }
 animate();
 
@@ -63,19 +83,41 @@ animate();
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
-            console.log('pressed w')
+            keys.w.pressed = true
             break 
 
         case 'a':
-            console.log('pressed a')
+            keys.a.pressed = true
             break 
 
         case 's':
-            console.log('pressed s')
+            keys.s.pressed = true
             break 
 
         case 'd':
-            console.log('pressed d')
+            keys.d.pressed = true
             break 
     }
+    console.log(keys)
+});
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break 
+
+        case 'a':
+            keys.a.pressed = false
+            break 
+
+        case 's':
+            keys.s.pressed = false
+            break 
+
+        case 'd':
+            keys.d.pressed = false
+            break 
+    }
+    console.log(keys)
 })
