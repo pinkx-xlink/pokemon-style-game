4cc01a4a264b9c1a66a25c95a672fea997724020
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-06-24 17:15:14 -0400

    Move the code for Boundary and Sprite classes from main.js to classes.js; add script tag for classes.js to index.html

--- main.js
+++ main.js
@@ -12,19 +12,7 @@ for (let i = 0; i < collisions.length; i+= 70) {
     collisionsMap.push(collisions.slice(i, 70 + i));
 }
 
-class Boundary {
-    static width = 48
-    static height = 48
-    constructor({ position }) {
-        this.position = position
-        this.width = 48
-        this.height = 48
-    }
-    draw() {
-        c.fillStyle = 'rgba(255, 0, 0, 0)';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
+
 const boundaries = [];
 const offset = {
     x: -1121,
@@ -58,38 +46,6 @@ image.src = './img/PokemonStyleGameMap1.png';
 const playerImage = new Image();
 playerImage.src = './img/playerDown.png';
 
-class Sprite {
-    constructor({ position, velocity, image, frames = { max: 1 } }) {
-        this.position = position
-        this.image = image
-        // this.velocity = velocity
-        this.frames = frames
-
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max
-            this.height = this.image.height
-            console.log(this.height)
-            console.log(this.width)
-        }
-        
-    }
-
-    draw() {
-        c.drawImage(
-        this.image,
-        // cropping
-        0,
-        0,
-        this.image.width / this.frames.max,
-        this.image.height,
-        // actual image rendered
-        this.position.x,
-        this.position.y,
-        this.image.width / this.frames.max,
-        this.image.height
-        )
-    }
-}
      // 
      // 
 
